d1f933a1c170f11a2697eb9775b840d017ba9a89
scumabo <boma312@gmail.com>
2020-05-20 14:18:53 -0400

    package rename

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup
 
-setup(name='guess_number',
-      version='0.1',
+setup(name='guess_number_bo',
+      version='0.0.2',
+      author='Bo Ma',
       description='Guess a number game',
-      packages=['guess_number'],
+      packages=['guess_number_bo'],
       zip_safe=False)
